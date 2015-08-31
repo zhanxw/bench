0cd600fc588bc7bca068299ac99235bea5e8dc9d
zhanxw <zhanxw@gmail.com>
2015-08-31 10:37:47 -0500

    partly done with graphics

--- scripts/monitor.py
+++ scripts/monitor.py
@@ -24,13 +24,96 @@ def printTable(d, sep = '\t', outFile = sys.stderr):
     for l in d.iterrows():
         print >> outFile, sep.join([str(i) for i in l[1]])
 
+def calculateMean(timePoint, value):
+    #print "tp = ", timePoint
+    #print "value = ", value
+    if len(value) == 1:
+        return value
+    value = value[0:-1]
+    weight = timePoint[1:] - timePoint[0:-1]
+    totalSpan = timePoint[-1] - timePoint[0]
+    if any((i < 0 for i in weight)):
+        print >> sys.stderr, "Timepoint is not monotonelly increasing!"
+        return 0.
+    if totalSpan == 0.0:
+        return value[0]
+    avg = np.sum(weight * value) / totalSpan
+    return avg
+        
+def draw(dRaw, dg, outFile):
+    import matplotlib
+    import matplotlib.pyplot as plt
+    fig = plt.figure(figsize = (18, 18))
+    dg['prog'] = dg.apply(lambda x: x['cmd'].split()[0] + '(%d)' % x['pid'] ,axis = 1)
+    dg.index = dg['prog']
+    ax = fig.add_subplot(3, 3, 1)
+    dg[['utime']].plot(kind = 'barh', title = "User Time (s)", ax = ax)
+    ax = fig.add_subplot(3, 3, 2)    
+    dg[['stime']].plot(kind = 'barh', title = "System Time (s)", ax = ax)
+    ax = fig.add_subplot(3, 3, 3)    
+    dg[['rtime']].plot(kind = 'barh', title = "Real Time (s)", ax = ax)
+    def scaleUnit(x):
+        if (x.max() > 1024 ** 3).all():
+            return ( 1024 ** 3, "Gb")
+        if (x.max() > 1024 **  2).all():
+            return ( 1024 ** 2, "Mb")
+        if (x.max() > 1024).all():
+            return ( 1024, "Kb")
+        return ( 1, "B") 
+    rssScale = scaleUnit(dRaw[['rss']])
+    dg[['maxRss']] = dg[['maxRss']] / rssScale[0]
+    ax = fig.add_subplot(3, 3, 4)    
+    dg[['maxRss']].plot(kind = 'barh', title = "Max RSS (" + rssScale[1]+")", ax = ax)
+    
+    dg[['avgRss']] = dg[['avgRss']] / rssScale[0]
+    ax = fig.add_subplot(3, 3, 5)
+    dg[['avgRss']].plot(kind = 'barh', title = "Avg RSS (" + rssScale[1]+")", ax = ax)
+    
+    vmsScale = scaleUnit(dRaw[['vms']])
+    dg[['maxVms']] = dg[['maxVms']] / vmsScale[0]
+    ax = fig.add_subplot(3, 3, 6)
+    dg[['maxVms']].plot(kind = 'barh', title = "Max VMS (" + vmsScale[1]+")", ax = ax)
+    
+    dg[['avgVms']] = dg[['avgVms']] / vmsScale[0]
+    ax = fig.add_subplot(3, 3, 7)
+    dg[['avgVms']].plot(kind = 'barh', title = "Avg VMS (" + vmsScale[1]+")", ax = ax)
+    
+
+    def calculateYLimit(x, coef = 1.5):
+        a, b = x.min(), x.max()
+        c = (a + b) / 2
+        d = c - a
+        return (c - d * 1.5, c + d * 1.5)
+        
+    dRaw['prog'] = dRaw.apply(lambda x: x['cmd'].split()[0] + '(%d)' % x['pid'] ,axis = 1)
+    dRaw['rss'] = dRaw['rss'] / rssScale[0]
+    ax = fig.add_subplot(3, 3, 8)        
+    for k, v in dRaw.groupby('prog'):
+        plt.plot(v['rtime'], v['rss'], label = k, marker = '.')
+
+    plt.ylim(calculateYLimit(dRaw['rss']))
+    plt.title("RSS (%s) vs. Real Time (s)" % rssScale[1])
+    plt.legend()
+    #plt.legend(bbox_to_anchor=(1.05, 1), loc = 2)
+
+    dRaw[['vms']] = dRaw[['vms']] / vmsScale[0]
+    ax = fig.add_subplot(3, 3, 9)        
+    for k, v in dRaw.groupby('prog'):
+        plt.plot(v['rtime'], v['vms'], label = k, marker = '.')
+    plt.ylim(calculateYLimit(dRaw['vms']))
+    plt.title("VMS (%s) vs. Real Time (s)" % vmsScale[1])
+    plt.legend()    
+    #plt.legend(bbox_to_anchor=(1.05, 1), loc = 2)
+
+    fig.savefig(outFile)
+    
 def usage():
     print("Usage: ")
     print("%s [-i interval] [-o outputFile] [-s] [-t] [-g] [-q] commands" % sys.argv[0] )
     print(" -i interval: sampling interval")
     print(" -o outputFile: benchmark summary printed to 'outputFile' (default: stderr)")
     print(" -t: output trace of benchmarking metrics (default: stderr; use -o to change)")
-    print(" -g: output a PNG graph showing cpu and memory usage (need matplotlib and numpy)")
+    print(" -g: output a PNG graph showing cpu and memory usage (need matplotlib)")
     print(" -q: quiet mode, do not output anything to the console")
     print
 
@@ -78,6 +161,9 @@ if __name__ == '__main__':
 
     import psutil
     import time
+    import numpy as np
+    import pandas as pd
+    
     from collections import deque
     startTime = time.time()
     mainProc = psutil.Popen(command, shell = False)
@@ -92,7 +178,7 @@ if __name__ == '__main__':
         mainProc.poll() 
         queue.append(mainProc)
         while len(queue) > 0:
-            # print "len(queue) = ", len(queue), queue
+            print "len(queue) = ", len(queue), queue
             p = queue.popleft()
 
             if not p.is_running():
@@ -140,7 +226,6 @@ if __name__ == '__main__':
             break
     
     # Summarize results
-    import pandas as pd
     df = pd.DataFrame.from_items([('pid', [i[1] for i in result]),
                        ('ppid', [i[2] for i in result]),
                        ('cwd', [i[3] for i in result]),
@@ -154,17 +239,28 @@ if __name__ == '__main__':
         df.to_csv(outFile + ".trace.csv", index = False)
         
     # Group by pid
-    dfUniq = df[['pid','ppid','cwd','cmd']].drop_duplicates()
-    dfUniq.index = dfUniq['pid']
-    dfMax = df[['pid','utime','stime', 'rtime', 'rss','vms']].groupby('pid').max()
-    dfMax.columns = ['utime', 'stime', 'rtime', 'maxRss', 'maxVms']
-    dfMean = df[['pid', 'rss','vms']].groupby('pid').mean()
-    dfMean.columns = ['avgRss', 'avgVms']
-    dOut = pd.concat([dfUniq, dfMax, dfMean], axis = 1)
+    def f(x):
+        tp = np.copy(x['rtime'])
+        x['utime'] = np.max(x['utime'])
+        x['stime'] = np.max(x['stime'])
+        x['rtime'] = np.max(x['rtime'])
+        x['maxRss'] = np.max(x['rss'])
+        x['maxVms'] = np.max(x['vms'])
+        x['avgRss'] = calculateMean(tp, x['rss'])
+        x['avgVms'] = calculateMean(tp, x['vms'])
+        return x
+    dOut = df.groupby('pid').apply(f)
+    dOut = dOut.drop_duplicates()
+    # dOut = pd.concat([dfUniq, dfMax, dfMean], axis = 1)
     # print dOut
     if outFile == sys.stderr:
         if not quietMode:
             printTable(dOut)
     else:
         dOut.to_csv(outFile + '.csv', index = False)
-    sys.exit(0)
+
+    if outGraph:
+        if outFile == sys.stderr:
+            draw(df, dOut, "bench.png")
+        else:
+            draw(df, dOut, outFile + ".png")
